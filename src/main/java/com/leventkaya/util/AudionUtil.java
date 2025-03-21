7e3ccebd2ca65f9bf55e801578114834fc4b430a
Levent Kaya <levent.kaya@codefirst.io>
2025-03-21 14:26:13 +0300

    [gui] changed theme

--- src/main/java/com/leventkaya/util/AudionUtil.java
+++ src/main/java/com/leventkaya/util/AudionUtil.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class AudionUtil {
     public static final String DATABASE_URL = "jdbc:sqlite:audion.db";
-    public static final String THEME_PATH = "/css/dracula.css";
+    public static final String THEME_PATH = "/css/nord-dark.css";
     public static final int DEFAULT_WIDTH = 640;
     public static final int DEFAULT_HEIGHT = 480;
 
